9116babb27446c618ae5ae95b8fc9580127e0935
Nishant Chintala <crnishant@gmail.com>
2020-05-02 14:33:51 -0400

    sorta got backend get working

--- src/projects/projectsController.ts
+++ src/projects/projectsController.ts
@@ -2,12 +2,49 @@ import express, { RequestHandler } from 'express';
 import { ProjectsModel } from './projectsModel';
 import { Database } from '../common/MongoDB';
 import { Config } from '../config';
+import { request } from 'http';
+//import { http, https} from 'follow-redirects';
 //This is just an example of a second controller attached to the security module
 
 export class ProjectsController {
     static db: Database = new Database(Config.url, "projects");
     static projectsTable = 'projects';
+    getHello(req: express.Request, res: express.Response) {
+        //https://wanago.io/2019/03/18/node-js-typescript-6-sending-http-requests-understanding-multipart-form-data/
+        //getaddrinfo ENOTFOUND https://energ.ee/covid19-us-api/states.json
+        //IncomingMessage {_readableState: ReadableState, readable: true, _events: Object, _eventsCount: 1, _maxListeners: undefined, …}
+        //https://stackoverflow.com/questions/7323932/how-do-you-follow-an-http-redirect-in-node-js
+        /*const data = request(
+            {
+              host: 'energ.ee',
+              path: '/covid19-us-api/states.json',
+              method: 'GET',
+            },
+            response => {
+              console.log(response.statusMessage); // 200
+              res.send(response.statusCode);
+            }
+          );
+           
+        data.end();
+        */
+       var http = require('follow-redirects').http;
+       var opts = {host: 'energ.ee', path:'/covid19-us-api/states.json'};
+       http.get(opts, (resp: express.Response) => {
+         var page = '';
+         resp.on('data', (chunk) => {
+           page += chunk;
+         });
+         resp.on('end', function() {
+            res.send(page);
+         });
+        });
+        //yarn add follow-redirects
+        //npm install follow-redirects
 
+        //res.send("GURTHQUAKE");
+    }
+    
     //getProjects
     //sends a json object with all projects in the system that match :year
     getProjects(req: express.Request, res: express.Response) {
